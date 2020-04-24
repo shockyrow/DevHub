e2113a6406d03657d6eb2e83b01e991660b51cd0
Ibrokhim Shokirov <ibrokhim.shokirov@gmail.com>
2020-04-25 01:30:44 +0700

    Added home and help page

---
+++ js/elements.js
@@ -0,0 +1,11 @@
+let elements = document.getElementsByTagName('element');
+let reader = new FileReader();
+console.log(elements);
+
+for (let element of elements) {
+    fetch(`/elements/${element.getAttribute('name')}.html`)
+        .then(response => response.text())
+        .then(text => {
+            element.outerHTML = text;
+        });
+}
