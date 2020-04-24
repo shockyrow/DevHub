2c006be53be63d2c6db510c6c14841324ee3e17d
Ibrokhim Shokirov <ibrokhim.shokirov@gmail.com>
2020-04-25 01:58:30 +0700

    Fixed 404 error

--- js/elements.js
+++ js/elements.js
@@ -3,7 +3,7 @@ let reader = new FileReader();
 console.log(elements);
 
 for (let element of elements) {
-    fetch(`../elements/${element.getAttribute('name')}.html`)
+    fetch(`elements/${element.getAttribute('name')}.html`)
         .then(response => response.text())
         .then(text => {
             element.outerHTML = text;
